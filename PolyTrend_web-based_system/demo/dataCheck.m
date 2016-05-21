c933533310c4ed516996d694b782253a7252a731
Wei Yufei <weiyufei2014@outlook.com>
2016-05-21 12:06:16 +0200

    Update dataCheck.m

--- PolyTrend_web-based_system/demo/dataCheck.m
+++ PolyTrend_web-based_system/demo/dataCheck.m
@@ -1,4 +1,4 @@
-%---------------------------------------------------------------------
+%---------------------------------------------------------------------------------------------
 %  Check if the time-series should be processed. Time-series with         
 %  any negative data value or with a low amplitude, indicating       
 %  values over see or desert, should not be processed.   
@@ -10,7 +10,7 @@
 %  Lars Eklundh, Lund University, Sweden 
 %  Email: lars.eklundh@nateko.lu.se  
 %
-%  Yufei Wei, Lund University, Sweden 
+%  Yufei Wei, Lund University
 %  Email: weiyufei2014@outlook.com  
 %  Copyright (C) 2016 Per J\"onsson, Lars Eklundh, Yufei Wei
 %  
@@ -24,18 +24,22 @@
 %  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 %  GNU General Public License for more details.
 %  
-%  You should have received a copy of the GNU General Public License
-%  along with this program.  If not, see <http://www.gnu.org/licenses/>.
-%---------------------------------------------------------------------
+%  For a copy of the GNU General Public License,
+%  see <http://www.gnu.org/licenses/>.  
+%---------------------------------------------------------------------------------------------
 function process = dataCheck(y)
 npt=size(y,2);% count the number of NDVI values
-dif=zeros(1,npt-1);% create a zero vector to store differences between each values comparing with its neighbors
-for i=1:npt-1
+dif=zeros(1,npt-1);% create a zero vector to store differences between each values comparing  
+                   % with its neighbors
+difpt=npt-1;% the number of the differences
+signY=sign(y);% get the sign of each element
+for i=1:difpt
     dif(i)=abs(y(i+1)-y(i));
 end
-if (sum(dif<1.e-6)>=3*npt/4)||any(sign(y)==-1)% If at least one NDVI values in the whole time span is smaller than 0,
-    process=0; %or if during the whole time span, the number of NDVI pixels that have a difference of less than 0.000001  
-else           % comparing to the ones that represent adjacent years is larger than or equal to 3 multiplies the number of 
-    process=1; % years of whole time span divided by 4,
-end            % then the whole vector is viewed as unqualified and will not be processed.
-end
+if (sum(dif<1.e-6)>=3*npt/4)||any(signY==-1)% If at least one NDVI values in the whole 
+                                            % time span is smaller than 0,
+    process=0; % or if during the whole time span, the number of NDVI pixels that have   
+else           % a difference of less than 0.000001 comparing to the ones that represent  
+    process=1; % adjacent years is larger than or equal to 3 times the number of years 
+end            % of the whole time span divided by 4, then the whole vector is viewed 
+end            % as unqualified and will not be processed.
